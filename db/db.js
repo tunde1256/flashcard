eaa2c55ade6a9b91c425967e17329bfc7c33fe1d
tunde1256 <109033697+tunde1256@users.noreply.github.com>
2024-09-20 09:57:27 +0100

    Add files via upload

--- db/db.js
+++ db/db.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
+DB_URI='mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
 
+// DB_URI= "mongodb+srv://ogunremitunde12:ogunremitunde12@cluster0.g3bnlww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 // Connect to MongoDB
-const connection = mongoose.connect(process.env.DB_URI, {
+const connection = mongoose.connect(DB_URI, {
     maxPoolSize: 10, // Maximum number of connections in the pool
     serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds trying to connect to the database
     socketTimeoutMS: 45000, // Timeout after 45 seconds of inactivity
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const cron = require('node-cron');  // Add this for task scheduling
+const { broadcastInactiveUsers } = require('./controller/admin'); // Import your broadcast logic
 const { swaggerUi, swaggerSpec } = require('./swagger'); // Import Swagger
 
 const app = express();
@@ -9,12 +11,12 @@ const port = process.env.PORT || 3000;
 
 const Db = require('./db/db');
 const UserRoute = require('./routes/userRouter');
-
 const AdminRoute = require('./routes/AdminRouter');
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 app.use(morgan('combined'));
+require('./scheduling');
 
 // Use CORS middleware
 app.use(cors());
@@ -25,6 +27,16 @@ app.use('/api/user', UserRoute);
 
 // Swagger UI setup
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Schedule the cron job to run daily at midnight (adjust timing as needed)
+// cron.schedule('0 0 * * *', async () => {
+//     try {
+//         console.log('Running automated broadcast to inactive users');
+//         await broadcastInactiveUsers();  // Run the broadcast function automatically
+//     } catch (error) {
+//         console.error('Error running automated broadcast', error);
+//     }
+// });
 
 // Start the server
 app.listen(port, () => {
